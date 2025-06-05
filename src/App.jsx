ed216e281858d99639ea7d43246b22fc6c7e9618
vinij-03 <vineetjana3@gmail.com>
2025-06-05 21:51:22 +0530

    Fix axios base URL by adding trailing slash

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { UserContextProvider } from './UserContext'
 
 function App() {
 
-  axios.defaults.baseURL = 'https://resonant-lillian-vineetjana-d2332314.koyeb.app';  
+  axios.defaults.baseURL = 'https://resonant-lillian-vineetjana-d2332314.koyeb.app/';  
 
   axios.defaults.withCredentials = true;
 
